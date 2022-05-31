281f10078c720e883d14591eb41c20e9b18c298a
Don Cryptus <don.cryptus@gmail.com>
2022-05-31 11:44:01 +0200

    initial commit

---
+++ src/main/java/don/savagescan/SavagescanApplication.java
@@ -0,0 +1,13 @@
+package don.savagescan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavagescanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavagescanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/don/savagescan/SavagescanApplicationTests.java
@@ -0,0 +1,13 @@
+package don.savagescan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavagescanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
