6c8ea3d0aa57881ee937371e0994f1cd8a23868c
Don-Cryptus <don.cryptus@gmail.com>
2022-08-29 15:31:29 +0200

    test vpn

--- src/main/java/don/savagescan/SavageScanApplication.java
+++ src/main/java/don/savagescan/SavageScanApplication.java
@@ -8,6 +8,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 @SpringBootApplication
 @RequiredArgsConstructor
 public class SavageScanApplication implements CommandLineRunner {
@@ -25,11 +31,24 @@ public class SavageScanApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        if (environment.equals("production")) {
-            savageScan.start();
-        } else {
-            scanCheck.check();
+
+
+        try {
+            URL yahoo = new URL("https://api.ipify.org/?format=text");
+            URLConnection yc = yahoo.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null)
+                System.out.println(inputLine);
+            in.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+//        if (environment.equals("production")) {
+//            savageScan.start();
+//        } else {
+//            scanCheck.check();
+//        }
     }
 
 }
