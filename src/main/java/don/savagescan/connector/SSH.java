9aa47c6b839301ed6265e12e32ee7f9d9e3353dd
Don-Cryptus <don.cryptus@gmail.com>
2022-09-02 17:18:14 +0200

    update memory overflow

--- src/main/java/don/savagescan/connector/SSH.java
+++ src/main/java/don/savagescan/connector/SSH.java
@@ -5,7 +5,6 @@ import lombok.NoArgsConstructor;
 import net.schmizz.sshj.SSHClient;
 import net.schmizz.sshj.common.IOUtils;
 import net.schmizz.sshj.connection.channel.direct.Session;
-import net.schmizz.sshj.connection.channel.direct.Session.Command;
 import net.schmizz.sshj.transport.verification.PromiscuousVerifier;
 import org.springframework.stereotype.Component;
 
@@ -16,7 +15,7 @@ import java.util.List;
 @Component
 @NoArgsConstructor
 public class SSH {
-    final SSHClient ssh = new SSHClient();
+
     private final String username = "root";
     private final int port = 22;
 
@@ -58,29 +57,33 @@ public class SSH {
 
     public void connect() {
 
-        try {
+        try (final SSHClient ssh = new SSHClient()) {
             ssh.addHostKeyVerifier(new PromiscuousVerifier());
             ssh.setTimeout(10000);
             ssh.setConnectTimeout(10000);
             ssh.connect(host, port);
             ssh.authPassword(username, password);
 
-            Session session = ssh.startSession();
-            validSession = ssh.isAuthenticated();
+            try (Session session = ssh.startSession()) {
+                validSession = ssh.isAuthenticated();
 
-            final Command cmd = session.exec("ssh -V");
-            message = IOUtils.readFully(cmd.getInputStream()).toString().toLowerCase() + IOUtils.readFully(cmd.getErrorStream()).toString().toLowerCase();
+                final Session.Command cmd = session.exec("ssh -V");
+                message = IOUtils.readFully(cmd.getInputStream()).toString().toLowerCase() + IOUtils.readFully(cmd.getErrorStream()).toString().toLowerCase();
 
-            sshState = message.contains("openssh");
+                sshState = message.contains("openssh");
 
-            session.close();
-            ssh.disconnect();
-        } catch (RuntimeException | IOException e) {
-
+            } catch (IOException e) {
+                if (e.getMessage() != null) {
+                    message = e.getMessage();
+                    validSession = e.getMessage().contains("Exhausted available authentication methods");
+                }
+            }
+        } catch (IOException e) {
             if (e.getMessage() != null) {
                 message = e.getMessage();
                 validSession = e.getMessage().contains("Exhausted available authentication methods");
             }
+
         }
     }
 
